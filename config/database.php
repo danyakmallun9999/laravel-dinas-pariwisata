541a58d8dfff012b4de6fbe51c6e2e7dde224cca
danyakmallun9999 <danyakmallun9999@gmail.com>
2025-12-18 21:10:24 +0700

    Fix database config key options

--- config/database.php
+++ config/database.php
@@ -96,7 +96,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
-            'attributes' => [
+            'options' => [
                 PDO::ATTR_EMULATE_PREPARES => true,
             ],
         ],
