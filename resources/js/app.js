a2964894abd2c2022c9b609db2f8b030110b5362
danyakmallun9999 <danyakmallun9999@gmail.com>
2026-01-30 10:41:25 +0700

    feat: Add explore map page with Leaflet, routing, and search functionality.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import L from 'leaflet';
 import maplibregl from 'maplibre-gl';
 window.L = L;
 import 'leaflet-draw';
+import 'leaflet-routing-machine';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'; // Import CSS
 window.maplibregl = maplibregl;
 
 import Alpine from 'alpinejs';
